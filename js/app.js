499dd5fec079b4602a6170e02c3dbbb2ed0927b4
Francis Gassert <cowbox314@gmail.com>
2015-07-14 14:00:04 -0400

    Clarify map layer labels

--- js/app.js
+++ js/app.js
@@ -3,19 +3,19 @@ $(document).ready(function() {
 L.mapbox.accessToken = 'pk.eyJ1Ijoid3JpIiwiYSI6Ik9TY2w5RTQifQ.0HV7dQTjK40mk7GpNNA64g';
   var map = L.mapbox.map('map', null, {
       maxZoom: 18
-  }).setView([30.0599153,31.2620199], 6);
+  }).setView([30.0599153,31.2620199], 4);
 
   var baseLayers = {
       'Mono': L.mapbox.tileLayer('wri.c974eefc'),
       'Context': L.mapbox.tileLayer('wri.b71b0f45')
   };
   var overlays = {
-      Aqueduct: L.tileLayer.wms("http://gis.wri.org/arcgis/services/Aqueduct/aqueduct_projections_2014/MapServer/WMSServer",{layers:'1', opacity: 0.5, transparent:true,format:'image/png'}),
-      Data: L.mapbox.tileLayer('devseed.malaria-color'),
-      English: L.mapbox.tileLayer('wri.acf5a04e'),
-      Locale: L.mapbox.tileLayer('wri.b677c4f8'),
-      CN: L.mapbox.tileLayer('wri.4d3dc5a3'),
-      IN: L.mapbox.tileLayer('wri.99740e02'),
+      'Example Data Layer': L.tileLayer.wms("http://gis.wri.org/arcgis/services/Aqueduct/aqueduct_projections_2014/MapServer/WMSServer",{layers:'1', opacity: 0.5, transparent:true,format:'image/png'}),
+      'Example Data Layer 2': L.mapbox.tileLayer('devseed.malaria-color'),
+      'English labels': L.mapbox.tileLayer('wri.acf5a04e'),
+      'Locale labels': L.mapbox.tileLayer('wri.b677c4f8'),
+      'China labels & boundaries': L.mapbox.tileLayer('wri.4d3dc5a3'),
+      'India labels & boundaries': L.mapbox.tileLayer('wri.99740e02'),
   };
 		
 
